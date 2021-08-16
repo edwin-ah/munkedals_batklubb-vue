1c13d2e5d7dc047c2d6766f5b8cc32960a0ccb4a
edan0003 <edwin.andersson-hallsten@student.hv.se>
2021-08-16 15:14:41 +0200

    test

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,7 +16,7 @@
         </div>
     <div class="wave-holder-bottom h-40"></div>
 
-    <!-- MODAL -->
+    <!-- MODAL d -->
     <div v-if="openModal" class="pt-24 h-screen w-screen shadow fixed left-0 top-0 z-10 overflow-auto bg-black bg-opacity-40">
         <div class="w-full max-w-sm mx-auto">
             <button @click="openModal = !openModal" class="float-right text-gray-700 text-xl font-bold bg-white mr-4 hover:text-black focus:text-black">
